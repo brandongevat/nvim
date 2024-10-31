b438085f31d36cf924285cbfe7bffc5de0d6c060
Brandon Gevat <brandongevat@gmail.com>
2024-10-31 15:07:35 -0400

    Add fugitive plugin for git

--- lua/brandongevat/remap.lua
+++ lua/brandongevat/remap.lua
@@ -9,6 +9,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- Maps the explorer command
 -- LSP
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)       -- Maps the current LSP formatter
 vim.keymap.set("n", "<leader>wa", vim.lsp.buf.code_action) -- Maps the current LSP code action
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)      -- Maps the current LSP rename
 
 -- Custom: Format & save
 vim.keymap.set("n", "<leader>fs", function()

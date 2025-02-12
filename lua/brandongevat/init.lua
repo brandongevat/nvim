f97ec4f0bf743ebcc4e6b79f8f444592c2715622
Brandon Gevat <brandongevat@gmail.com>
2025-02-11 22:10:53 -0500

    convert to lazy plugin

--- lua/brandongevat/init.lua
+++ lua/brandongevat/init.lua
@@ -2,3 +2,4 @@ require("brandongevat.remap")       -- Sources the file remap.lua
 require("brandongevat.lazy-config") -- Sources the file lazy-config.lua
 require("brandongevat.opt")         -- Sources the file opt.lua
 require("brandongevat.wo")          -- Sources the file wo.lua
+require("brandongevat.lazy")
---
+++ lua/brandongevat/lazy/init.lua
@@ -0,0 +1,4 @@
+require("brandongevat.lazy.colors")
+require("brandongevat.lazy.fugitive")
+require("brandongevat.lazy.telescope")
+require("brandongevat.lazy.treesitter")

c374bea8536ddfd1023360f811d52e7b8797808d
brandon.gevat1 <brandon.gevat@eizo.com>
2025-03-20 11:46:46 -0400

    Add ocs52 for clipboard for ssh/docker passthrough

--- lua/brandongevat/lazy-config.lua
+++ lua/brandongevat/lazy-config.lua
@@ -23,6 +23,32 @@ return require('lazy').setup({
 	},
 
 	{
+		'ojroques/nvim-osc52',
+		config = function()
+			require('osc52').setup {
+				max_length = 0,
+				silent = false,
+				trim = false,
+				tmux_passthrough = false,
+			}
+
+			local function copy()
+				if vim.v.event.operator == 'y' and vim.v.event.regname == '+' then
+					require('osc52').copy_register('+')
+				end
+				if vim.v.event.operator == 'y' and vim.v.event.regname == '' then
+					require('osc52').copy_register('"')
+				end
+			end
+
+			vim.api.nvim_create_autocmd('TextYankPost', {
+				pattern = '*',
+				callback = copy,
+			})
+		end
+	},
+
+	{
 		'nvim-telescope/telescope.nvim',
 		tag = '0.1.8',
 		-- or                            , branch = '0.1.x',

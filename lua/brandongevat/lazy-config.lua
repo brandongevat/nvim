9cfb89f14f34b500b8f21cd1914bcc08d95e2d69
Brandon Gevat <brandongevat@gmail.com>
2025-12-08 23:46:39 -0500

    Update neovim config:

--- lua/brandongevat/lazy-config.lua
+++ lua/brandongevat/lazy-config.lua
@@ -18,6 +18,14 @@ vim.g.maplocalleader = ' '
 
 return require('lazy').setup({
 	{
+		"3rd/image.nvim",
+		build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+		opts = {
+			processor = "magick_cli",
+		}
+	},
+
+	{
 		"iamcco/markdown-preview.nvim",
 		ft = { "markdown" },
 		build = function()
